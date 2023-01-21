804003ade77672c35a861878d58e3e0ca6ff5e26
Oleg Andreyev <oleg@andreyev.lv>
2023-01-22 00:13:56 +0200

    running simple-phpunit for macos

--- tests/Custom/PromptTest.php
+++ tests/Custom/PromptTest.php
@@ -29,10 +29,6 @@ class PromptTest extends TestCase
 
     public function testPromptDismiss(): void
     {
-        if (getenv('BROWSER_NAME') === 'safari') {
-            $this->markTestSkipped('\OAndreyev\Mink\Tests\Driver\Custom\PromptTest::testPromptDismiss is skipped due to weird issue with Safari');
-        }
-
         $session = $this->getSession();
         $session->visit($this->pathTo('/prompt.html'));
         /** @var WebDriver $driver */
