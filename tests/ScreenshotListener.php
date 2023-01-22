616aba2bb23803b0b1805d5b8326e29f20b29a01
Oleg Andreyev <oleg@andreyev.lv>
2023-01-22 02:48:17 +0200

    disabled screenshot for safari

--- tests/ScreenshotListener.php
+++ tests/ScreenshotListener.php
@@ -59,6 +59,10 @@ class ScreenshotListener implements TestListener
 
     private function makeScreenshot(Test $test): void
     {
+        if (getenv('BROWSER_NAME') === 'safari') {
+            return;
+        }
+
         try {
             /** @var Session $session */
             $session = \Closure::bind(function () {
